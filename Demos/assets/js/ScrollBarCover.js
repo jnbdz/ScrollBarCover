5bcae906ef29899e13bb4ec3c0fcdd89e599d8b4
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-05-04 21:54:47 -0400

    first commit

---
+++ Demos/assets/js/ScrollBarCover.js
@@ -0,0 +1,79 @@
+/*
+---
+description: It covers the scrollbar with a slider so it can be styled.
+
+authors:
+  - Jean-Nicolas Boulay Desjardins (http://jean-nicolas.name)
+  - Garrick Cheung (http://www.twitter.com/garrickcheung)
+
+license:
+  - MIT-style license
+
+requires:
+ - core/1.3:   '*'
+ - more/1.2.4.4 [Slider]
+ - ScrollSpy/1.5
+
+provides:
+  - ScrollBarCover
+...
+*/
+
+var ScrollBarCover = new Class({
+        
+        /* implements */
+        Implements: [Events],
+        
+        /* initialization */
+        initialize: function(container, options) {
+            /* container exists? */
+            this.container = container = document.id(container);
+
+            this.container.setStyles({
+                'float': 'left',
+                'display': 'inline',
+                'overflow': 'auto',
+                'margin-right': -20,
+                'padding-right': 20
+            });
+          
+            this.containerSize = this.container.getSize();
+
+	    this.sliderContainer = new Element('div', {
+                id: 'scrollbar-slider-container',
+                styles: {
+                    'position': 'relative',
+                    'width': 16,
+                    'z-index': 999,
+                    'float': 'right'
+                }
+            }).grab(new Element('div', {
+                id: 'scrollbar-slider-element'
+            }).grab(new Element('div', {
+                id: 'scrollbar-slider-knob'
+            })));
+
+            new Element('div', {
+                id: 'scrollbar-cover-wraper',
+                styles: {
+                    width: this.containerSize.x,
+                    height: this.containerSize.y
+                }
+            }).grab(this.sliderContainer.setStyle('height', this.containerSize.y)).wraps(this.container);	
+
+            this.sliderSteps = this.container.getScrollSize().y - this.container.getStyle('height').toInt();
+
+            var slider = new Slider('scrollbar-slider-element', 'scrollbar-slider-knob', {
+                                mode: 'vertical',
+                                steps: this.sliderSteps
+                        }).addEvent('change', function(position){
+                                this.container.scrollTo(0, position);
+                        }.bind(this));
+
+            new ScrollSpy({container: this.container}).addEvent('scroll', function(position){
+                slider.set(position.y);
+            });
+            
+        }
+    
+    });
---
+++ README.md
@@ -0,0 +1,27 @@
+ScrollBarCover
+===========
+
+This plugin is made to cover the scrollbar with a slider so you can style it.
+
+How to use
+----------
+
+JavaScript:
+
+new ScrollBarCover('the_id_of_the_element_or_the_body');
+
+CSS:
+
+<style>
+
+	#scrollbar-slider-element {
+        	width: 16px;
+        	background-color: #ccc;
+	}
+	#scrollbar-slider-knob {
+        	height: 16px;
+        	width: 16px;
+        	background-color: #000;
+	}
+
+</style>
